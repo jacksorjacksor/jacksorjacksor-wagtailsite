5fa6e48f20f05d8a3334f6c08c1036f2860a5dbd
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-20 17:22:07 +0100

    "UndefinedError: 'load' is undefined"

--- jacksorjacksor/settings/base.py
+++ jacksorjacksor/settings/base.py
@@ -67,6 +67,21 @@ ROOT_URLCONF = "jacksorjacksor.urls"
 
 TEMPLATES = [
     {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(PROJECT_DIR, "templates"),
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+    {
         "BACKEND": "django.template.backends.jinja2.Jinja2",
         "APP_DIRS": True,
         "OPTIONS": {
