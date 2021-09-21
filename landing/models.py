6c78760e286773026fe6c3378e6757ddbc0d6365
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-21 11:52:48 +0100

    Menu works!!

--- landing/models.py
+++ landing/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 from wagtail.core.models import Page
+from main_card_container.models import ListPage, DetailPage
 
 # Create your models here.
 
+
 class LandingPage(Page):
-    pass
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        context["music_list_page"] = ListPage.objects.filter(title="Music")[0]
+        context["tech_list_page"] = ListPage.objects.filter(title="Tech")[0]
+
+        print(context)
+        return context
