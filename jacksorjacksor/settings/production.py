53c7537f1d3525ab8e4af8177e98ebb0ce05cca0
Richard Jackson (laptop) <rjackson87@gmail.com>
2021-09-22 17:27:51 +0100

    added a secret key

--- jacksorjacksor/settings/production.py
+++ jacksorjacksor/settings/production.py
@@ -1,6 +1,9 @@
+from jacksorjacksor.settings.dev import SECRET_KEY
 from .base import *
 
 DEBUG = False
+
+SECRET_KEY = 'hrad1^10y^r+ne_l1p96()@d46=58-k&7+$c+zt)d-55uwh%ij'
 
 try:
     from .local import *
