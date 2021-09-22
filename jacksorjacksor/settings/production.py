f24c20d69075dbf9b258e01ed40a9eeba1ca8a8a
Richard Jackson (laptop) <rjackson87@gmail.com>
2021-09-22 17:30:55 +0100

    allowed_hosts as a list

--- jacksorjacksor/settings/production.py
+++ jacksorjacksor/settings/production.py
@@ -5,7 +5,9 @@ DEBUG = False
 
 SECRET_KEY = "hrad1^10y^r+ne_l1p96()@d46=58-k&7+$c+zt)d-55uwh%ij"
 
-ALLOWED_HOSTS = "jacksorjacksor.eu.pythonanywhere.com"
+ALLOWED_HOSTS = [
+    "jacksorjacksor.eu.pythonanywhere.com",
+]
 try:
     from .local import *
 except ImportError:
