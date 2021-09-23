231a73851873d9e7fbdcec3615d611eeff817a4b
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-23 17:43:07 +0100

    Have actually got it working...!

--- mailing_list/views.py
+++ mailing_list/views.py
@@ -5,11 +5,17 @@ from mailing_list.models import MailingListForm
 
 
 def mailing_list(request):
+    print("received!")
     data_from_post = json.load(request)["post_data"]
 
     if request.method == "POST":
-        f = MailingListForm(data_from_post)
-        f.save()
+        if len(data_from_post) > 0:
+            print("here though?")
+            print(data_from_post)
+            f = MailingListForm({"email_address": data_from_post})
+            f.save()
+            #     MailingListForm(data_from_post).save()
+
         # code that generates "output" variable
         output = "YOU'RE MY WIFE NOW DAVE!"
         data = {
