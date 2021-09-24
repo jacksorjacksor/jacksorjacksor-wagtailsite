73ede008b8b244d07c58f655b1089184ba5fc7aa
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-24 15:12:40 +0100

    Added more allowed hosts

--- jacksorjacksor/settings/production.py
+++ jacksorjacksor/settings/production.py
@@ -7,6 +7,8 @@ SECRET_KEY = "hrad1^10y^r+ne_l1p96()@d46=58-k&7+$c+zt)d-55uwh%ij"
 
 ALLOWED_HOSTS = [
     "jacksorjacksor.eu.pythonanywhere.com",
+    "www.jacksorjacksor.xyz",
+    "jacksorjacksor.xyz",
 ]
 try:
     from .local import *
--- mailing_list/views.py
+++ mailing_list/views.py
@@ -5,16 +5,11 @@ from mailing_list.models import MailingListForm
 
 
 def mailing_list(request):
-    print("received!")
     data_from_post = json.load(request)["post_data"]
-
+    # Then add validation etc.
     if request.method == "POST":
-        if len(data_from_post) > 0:
-            print("here though?")
-            print(data_from_post)
-            f = MailingListForm({"email_address": data_from_post})
-            f.save()
-            #     MailingListForm(data_from_post).save()
+        f = MailingListForm({"email_address": data_from_post})
+        f.save()
 
         # code that generates "output" variable
         output = "YOU'RE MY WIFE NOW DAVE!"
