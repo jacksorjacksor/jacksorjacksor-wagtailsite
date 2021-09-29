db4f49eb3934f8952d1ee04aa808580cf256fc9f
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-29 06:47:20 +0100

    More tests

--- jacksorjacksor/settings/production.py
+++ jacksorjacksor/settings/production.py
@@ -1,15 +1,8 @@
-from jacksorjacksor.settings.dev import ALLOWED_HOSTS, SECRET_KEY
+from jacksorjacksor.settings.dev_1 import ALLOWED_HOSTS, SECRET_KEY
 from .base import *
 
 DEBUG = False
 
-SECRET_KEY = "hrad1^10y^r+ne_l1p96()@d46=58-k&7+$c+zt)d-55uwh%ij"
-
-ALLOWED_HOSTS = [
-    "jacksorjacksor.eu.pythonanywhere.com",
-    "www.jacksorjacksor.xyz",
-    "jacksorjacksor.xyz",
-]
 try:
     from .local import *
 except ImportError:
