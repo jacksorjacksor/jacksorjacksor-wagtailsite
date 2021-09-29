7b4b0372167f317a222985443d1984dafb224378
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-29 06:32:02 +0100

    trying contact form

--- jacksorjacksor/settings/base.py
+++ jacksorjacksor/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "typing_extensions",
     "generic_page",
+    "contact",
     "wagtail.contrib.routable_page",
     "wagtail.contrib.modeladmin",
     "wagtailmenus",
