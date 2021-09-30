2891495593a4b4f69ec473d3a33cd5c038dee938
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-30 16:29:58 +0100

    Python commit

--- git_push.py
+++ git_push.py
@@ -1,5 +1,15 @@
 import git
 
+## Separate action as we'll need to name the commit, BUT!
+# Use psycopg2 to make pg_dump
+# Add all
+
 repo = git.Repo(".")
+repo.git.add(all=True)
+
+commit_message = input("Commit message:")
+
+repo.git.commit("-m", commit_message)
+
 origin = repo.remote(name="origin")
 origin.push()
