7a2a5716f9d9b7a87ed0d99017ff8c616fae5815
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-30 12:59:33 +0100

    Another change

--- webhooks/views.py
+++ webhooks/views.py
@@ -13,7 +13,7 @@ def webhook_update(request):
     print("So this will then PULL when a PUSH event happens")
     print("and we need to look into Python Decoupling")
     print("So then we have to:")
-    print("git pull")  # hi
+    print("git pull")
 
     print("restart server via touch wsgi")
     repo = git.Repo("jacksorjacksor-wagtailsite")
