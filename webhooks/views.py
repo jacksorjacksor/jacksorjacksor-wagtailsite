96fe039d582892f38b83d1aa7b4ce67718334b73
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-30 11:55:14 +0100

    More

--- webhooks/views.py
+++ webhooks/views.py
@@ -4,6 +4,6 @@ import git
 
 # Create your views here.
 def webhook_update(request):
-    print("Anything?")
+    file = open("victory.yes", "w+")
     if request.method == "POST":
         print("IT'S GIT UPDATE BABY")
