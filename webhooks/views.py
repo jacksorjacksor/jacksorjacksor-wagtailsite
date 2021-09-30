ccb7b71e141c754312521cc62a276d3de6970fd9
jacksorjacksor <rich@richardjacksonmusic.com>
2021-09-30 13:58:48 +0100

    Added 'touch wsgi' command

--- webhooks/views.py
+++ webhooks/views.py
@@ -3,20 +3,19 @@ from django.http import request, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import git
+import subprocess
 
 # Create your views here.
 # @require_POST
 @csrf_exempt
 def webhook_update(request):
-    print("AUTOMATIC BABY YEAHHHHHHHHHHH")
-    print("IT'S GIT UPDATE BABY")
-    print("So this will then PULL when a PUSH event happens")
-    print("and we need to look into Python Decoupling")
-    print("So then we have to:")
-    print("git pull")
 
-    print("restart server via touch wsgi")
     repo = git.Repo("jacksorjacksor-wagtailsite")
     origin = repo.remote(name="origin")
     origin.pull()
+
+    try:
+        subprocess.run(["touch", "/var/www/www_jacksorjacksor_xyz_wsgi.py"])
+    except:
+        print("couldn't restart server")
     return HttpResponse("<h1>HI!</h1>")
