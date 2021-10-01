df6076eb15e9aa1df684ec6ef0bd641a93574452
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-01 10:40:12 +0100

    Think I should get an email now...

--- git_push_and_update_db.py
+++ git_push_and_update_db.py
@@ -1,7 +1,6 @@
 import git
 import subprocess
 
-
 # Adds, commits and pushes to named repo:
 repo = git.Repo("~/wagtail/jacksorjacksor-wagtailsite")
 repo.git.add(all=True)
@@ -9,7 +8,6 @@ commit_message = input("Commit message: ")
 repo.git.commit("-m", commit_message)
 origin = repo.remote(name="origin")
 origin.push()
-
 
 # def database_update():
 #     psql_command = "pg_dump --host=localhost --port=5433 --username=jacksorjacksor --format=tar --file=database_dump.tar dbname=jacksorjacksor"
