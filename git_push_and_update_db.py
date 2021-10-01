8075cf80d2818cb2acb385bf191758ffac33c850
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-01 10:09:20 +0100

    test!

--- git_push_and_update_db.py
+++ git_push_and_update_db.py
@@ -2,10 +2,10 @@ import git
 import subprocess
 
 
-def database_update():
-    psql_command = "pg_dump --host=localhost --port=5433 --username=jacksorjacksor --format=tar --file=database_dump.tar dbname=jacksorjacksor"
-    psql_command_as_list = psql_command.split(" ")
-    subprocess.run(psql_command_as_list)
+# def database_update():
+#     psql_command = "pg_dump --host=localhost --port=5433 --username=jacksorjacksor --format=tar --file=database_dump.tar dbname=jacksorjacksor"
+#     psql_command_as_list = psql_command.split(" ")
+#     subprocess.run(psql_command_as_list)
 
 
 # Makes copy of Database as a directory file (password in .pgaccess)
