630fae4afc95ffd7fefc3ef62ea6c533feebb042
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-01 10:38:50 +0100

    test

--- git_push_and_update_db.py
+++ git_push_and_update_db.py
@@ -2,6 +2,15 @@ import git
 import subprocess
 
 
+# Adds, commits and pushes to named repo:
+repo = git.Repo("~/wagtail/jacksorjacksor-wagtailsite")
+repo.git.add(all=True)
+commit_message = input("Commit message: ")
+repo.git.commit("-m", commit_message)
+origin = repo.remote(name="origin")
+origin.push()
+
+
 # def database_update():
 #     psql_command = "pg_dump --host=localhost --port=5433 --username=jacksorjacksor --format=tar --file=database_dump.tar dbname=jacksorjacksor"
 #     psql_command_as_list = psql_command.split(" ")
@@ -11,11 +20,3 @@ import subprocess
 # Makes copy of Database as a directory file (password in .pgaccess)
 # database_update()
 # - No longer doing this as would wipe out any form data! Whoops!
-
-# Adds, commits and pushes to named repo:
-repo = git.Repo("~/wagtail/jacksorjacksor-wagtailsite")
-repo.git.add(all=True)
-commit_message = input("Commit message: ")
-repo.git.commit("-m", commit_message)
-origin = repo.remote(name="origin")
-origin.push()
--- webhooks/views.py
+++ webhooks/views.py
@@ -8,13 +8,13 @@ from django.core.mail import send_mail
 
 def send_email_to_me(reason):
     pass
-    # send_mail(
-    #     f"jacksorjacksor - site issue: {reason}",
-    #     f"Issue with {reason}",
-    #     "rich@jacksorjacksor.xyz",
-    #     ["jacksorjacksor@pm.me"],
-    #     fail_silently=False,
-    # )
+    send_mail(
+        f"jacksorjacksor - site issue: {reason}",
+        f"Issue with {reason}",
+        "rich@jacksorjacksor.xyz",
+        ["jacksorjacksor@pm.me"],
+        fail_silently=False,
+    )
 
 
 ## UTIL FUNCTIONS
@@ -33,18 +33,6 @@ def print_issue(command):
     return None
 
 
-def database_restore():
-    command = "pg_restore"
-    try:
-        print_running(command)
-        psql_command = "pg_restore --create --clean --host=jacksorjacksor-119.postgres.eu.pythonanywhere-services.com --port=10119 --no-password --dbname=jacksorjacksor --format=tar --username=jacksorjacksor --no-password /home/jacksorjacksor/jacksorjacksor-wagtailsite/database_dump.tar"
-        psql_command_as_list = psql_command.split(" ")
-        subprocess.run(psql_command_as_list)
-        print_completed(command)
-    except:
-        print_issue(command)
-
-
 # Create your views here.
 # @require_POST # This didn't work for some reason, but OK!
 @csrf_exempt
@@ -54,16 +42,6 @@ def webhook_update(request):
 
     send_email_to_me("hi!!!")
 
-    # command = "git stash"
-    # try:
-    #     print_running(command)
-    #     repo.git.add(all=True)
-    #     repo.git.stash()
-    #     repo.git.stash("drop")
-    #     print_completed(command)
-    # except:
-    #     print_issue(command)
-
     command = "git pull"
     try:
         print_running(command)
@@ -72,39 +50,6 @@ def webhook_update(request):
         print_completed(command)
     except:
         send_email_to_me("git pull")
-        print_issue(command)
-
-    # database_restore()
-    # Removed this as would wipe out existing materials
-
-    command = "activate venv"
-    try:
-        print_running(command)
-        run_static_command = "source /home/jacksorjacksor/.virtualenvs/wagtail/bin/activate"
-        run_static_command_as_list = run_static_command.split(" ")
-        subprocess.run(run_static_command_as_list)
-        print_completed(command)
-    except:
-        print_issue(command)
-
-    command = "check django"
-    try:
-        print_running(command)
-        run_static_command = "python ~/wagtail/jacksorjacksor-wagtailsite/manage.py check"
-        run_static_command_as_list = run_static_command.split(" ")
-        subprocess.run(run_static_command_as_list)
-        print_completed(command)
-    except:
-        print_issue(command)
-
-    command = "activate venv"
-    try:
-        print_running(command)
-        run_static_command = "python ~/wagtail/jacksorjacksor-wagtailsite/manage.py collectstatic --noinput"
-        run_static_command_as_list = run_static_command.split(" ")
-        subprocess.run(run_static_command_as_list)
-        print_completed(command)
-    except:
         print_issue(command)
 
     command = "restart server"
@@ -118,3 +63,58 @@ def webhook_update(request):
     print("Done!")
 
     return HttpResponse("<h1>HI!</h1>")  # probably should have something else here...
+
+    # database_restore()
+    # Removed this as would wipe out existing materials
+
+    # command = "activate venv"
+    # try:
+    #     print_running(command)
+    #     run_static_command = "source /home/jacksorjacksor/.virtualenvs/wagtail/bin/activate"
+    #     run_static_command_as_list = run_static_command.split(" ")
+    #     subprocess.run(run_static_command_as_list)
+    #     print_completed(command)
+    # except:
+    #     print_issue(command)
+
+    # command = "check django"
+    # try:
+    #     print_running(command)
+    #     run_static_command = "python ~/wagtail/jacksorjacksor-wagtailsite/manage.py check"
+    #     run_static_command_as_list = run_static_command.split(" ")
+    #     subprocess.run(run_static_command_as_list)
+    #     print_completed(command)
+    # except:
+    #     print_issue(command)
+
+    # command = "activate venv"
+    # try:
+    #     print_running(command)
+    #     run_static_command = "python ~/wagtail/jacksorjacksor-wagtailsite/manage.py collectstatic --noinput"
+    #     run_static_command_as_list = run_static_command.split(" ")
+    #     subprocess.run(run_static_command_as_list)
+    #     print_completed(command)
+    # except:
+    #     print_issue(command)
+
+    # command = "git stash"
+    # try:
+    #     print_running(command)
+    #     repo.git.add(all=True)
+    #     repo.git.stash()
+    #     repo.git.stash("drop")
+    #     print_completed(command)
+    # except:
+    #     print_issue(command)
+
+
+# def database_restore():
+#     command = "pg_restore"
+#     try:
+#         print_running(command)
+#         psql_command = "pg_restore --create --clean --host=jacksorjacksor-119.postgres.eu.pythonanywhere-services.com --port=10119 --no-password --dbname=jacksorjacksor --format=tar --username=jacksorjacksor --no-password /home/jacksorjacksor/jacksorjacksor-wagtailsite/database_dump.tar"
+#         psql_command_as_list = psql_command.split(" ")
+#         subprocess.run(psql_command_as_list)
+#         print_completed(command)
+#     except:
+#         print_issue(command)
