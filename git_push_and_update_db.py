4fa27d76340da1d26883f6ee24adb1ecb58921c4
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-01 06:38:32 +0100

    basic formatting

--- git_push_and_update_db.py
+++ git_push_and_update_db.py
@@ -1,21 +1,15 @@
 import git
 import subprocess
 
-# Database dump:
+# Makes copy of Database as a tar file (password in .pgaccess)
 psql_command = "pg_dump --host=localhost --port=5433 --username=jacksorjacksor --format=tar --file=database_dump dbname=jacksorjacksor"
-
 psql_command_as_list = psql_command.split(" ")
 subprocess.run(psql_command_as_list)
 
-## Separate action as we'll need to name the commit, BUT!
-# Use psycopg2 to make pg_dump
-
+# Adds, commits and pushes to named repo:
 repo = git.Repo("~/wagtail/jacksorjacksor-wagtailsite")
 repo.git.add(all=True)
-
 commit_message = input("Commit message: ")
-
 repo.git.commit("-m", commit_message)
-
 origin = repo.remote(name="origin")
 origin.push()
