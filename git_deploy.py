be96b56494bb45c6251a3c6308202b581e3aef2b
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-13 22:11:32 +0100

    Final thing

--- git_deploy.py
+++ git_deploy.py
@@ -18,6 +18,7 @@ If run from command line:
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--local", help="only runs the code locally, not remotely", action="store_true")
 parser.add_argument("-r", "--remote", help="only runs the code remotely, not locally", action="store_true")
+parser.add_argument("-p", "--pip", help="also runs pip install -r requirements.txt action", action="store_true")
 args = parser.parse_args()
 
 if args.local or not args.remote:
@@ -34,6 +35,12 @@ if args.remote or not args.local:
     print("REMOTE")
     # Remotely request the collectstatic (will this interfere with the git pull?)
     result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run(
-        "workon wagtail && cd jacksorjacksor-wagtailsite && git pull && python manage.py collectstatic --noinput && pip install -r requirements.txt"
+        "workon wagtail && cd jacksorjacksor-wagtailsite && git pull && python manage.py collectstatic --noinput"
     )
+
+    if args.pip:
+        result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run(
+            "workon wagtail && cd jacksorjacksor-wagtailsite && git pull && pip install -r requirements.txt"
+        )
+
     result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run("touch /var/www/www_jacksorjacksor_xyz_wsgi.py")
