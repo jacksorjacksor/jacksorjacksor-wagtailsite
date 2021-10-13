6e3545a3bbf69ac59a50bec0d76c47e1b57afd2e
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-13 22:09:23 +0100

    Made a python script yay!!!

--- git_deploy.py
+++ git_deploy.py
@@ -8,12 +8,19 @@ LOCAL: runs git add/commit/push
 REMOTE: runs git pull & python manage.py collectstatic & requirements
 """
 
+# COMMAND LINE SETTINGS
+"""
+If run from command line:
+-l: local only
+-r: remote only
+"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--local", help="only runs the code locally, not remotely", action="store_true")
 parser.add_argument("-r", "--remote", help="only runs the code remotely, not locally", action="store_true")
 args = parser.parse_args()
 
-if args.local:
+if args.local or not args.remote:
     print("LOCAL")
     # Adds, commits and pushes to named repo:
     repo = git.Repo("~/wagtail/jacksorjacksor-wagtailsite")
@@ -23,9 +30,10 @@ if args.local:
     origin = repo.remote(name="origin")
     origin.push()
 
-if args.remote:
+if args.remote or not args.local:
     print("REMOTE")
     # Remotely request the collectstatic (will this interfere with the git pull?)
     result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run(
         "workon wagtail && cd jacksorjacksor-wagtailsite && git pull && python manage.py collectstatic --noinput && pip install -r requirements.txt"
     )
+    result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run("touch /var/www/www_jacksorjacksor_xyz_wsgi.py")
