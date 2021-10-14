50f85c5e8fdf5dc24af584e9d3b23c88e6c5f59a
jacksorjacksor <rich@richardjacksonmusic.com>
2021-10-14 10:42:27 +0100

    more adjustments

--- git_deploy.py
+++ git_deploy.py
@@ -13,12 +13,13 @@ REMOTE: runs git pull & python manage.py collectstatic & requirements
 If run from command line:
 -l: local only
 -r: remote only
+-np: doesn't run pip install -r requirements.txt
 """
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--local", help="only runs the code locally, not remotely", action="store_true")
 parser.add_argument("-r", "--remote", help="only runs the code remotely, not locally", action="store_true")
-parser.add_argument("-p", "--pip", help="also runs pip install -r requirements.txt action", action="store_true")
+parser.add_argument("-np", "--nopip", help="doesn't run 'pip install -r requirements.txt'", action="store_true")
 args = parser.parse_args()
 
 if args.local or not args.remote:
@@ -33,14 +34,11 @@ if args.local or not args.remote:
 
 if args.remote or not args.local:
     print("REMOTE")
-    # Remotely request the collectstatic (will this interfere with the git pull?)
-    result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run(
-        "workon wagtail && cd jacksorjacksor-wagtailsite && git pull && python manage.py collectstatic --noinput"
-    )
+    with Connection("jacksorjacksor@ssh.eu.pythonanywhere.com") as c:
+        c.run("workon wagtail && cd jacksorjacksor-wagtailsite && git pull && python manage.py collectstatic --noinput")
+        c.run("workon wagtail && cd jacksorjacksor-wagtailsite && git pull && python manage.py migrate")
 
-    if args.pip:
-        result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run(
-            "workon wagtail && cd jacksorjacksor-wagtailsite && git pull && pip install -r requirements.txt"
-        )
+        if not args.nopip:
+            c.run("workon wagtail && cd jacksorjacksor-wagtailsite && git pull && pip install -r requirements.txt")
 
-    result = Connection("jacksorjacksor@ssh.eu.pythonanywhere.com").run("touch /var/www/www_jacksorjacksor_xyz_wsgi.py")
+        c.run("touch /var/www/www_jacksorjacksor_xyz_wsgi.py")
